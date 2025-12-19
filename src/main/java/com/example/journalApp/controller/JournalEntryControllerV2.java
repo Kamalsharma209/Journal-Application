32e1153e0a7f4b78d8d2ba8cd7ea40bb102d0f50
Your Name <Kamalsharma4769@gmail.com>
2025-12-20 01:52:48 +0530

    Using ResposneEntity to handle HTTP Status

--- src/main/java/com/example/journalApp/controller/JournalEntryControllerV2.java
+++ src/main/java/com/example/journalApp/controller/JournalEntryControllerV2.java
@@ -4,10 +4,13 @@ import com.example.journalApp.entity.JournalEntry;
 import com.example.journalApp.service.JournalEntryService;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/journal")
@@ -19,38 +22,58 @@ public class JournalEntryControllerV2 {
 
 
     @GetMapping("/getentries")
-    public List<JournalEntry> getAll(){
-
-        return journalEntryService.getAll();
+    public ResponseEntity<?>getAll(){
+        List<JournalEntry> all = journalEntryService.getAll();
+        if(all != null && !all.isEmpty() ){
+            return new ResponseEntity<>(all,HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
     @PostMapping("/postentries")
-    public JournalEntry createEntry(@RequestBody JournalEntry myEntry){
-        myEntry.setDate(LocalDateTime.now());
-        journalEntryService.saveEntry(myEntry);
-        return myEntry;
+    public ResponseEntity<JournalEntry> createEntry(@RequestBody JournalEntry myEntry){
+        try{
+            myEntry.setDate(LocalDateTime.now());
+            journalEntryService.saveEntry(myEntry);
+            return new ResponseEntity<>(myEntry,HttpStatus.CREATED);
+
+        } catch(Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @GetMapping("/id/{myId}")
-    public JournalEntry getJournalEntries(@PathVariable ObjectId myId){
+    public ResponseEntity<JournalEntry> getJournalEntries(@PathVariable String myId) {
 
-        return journalEntryService.findById(myId).orElse(null);
+        ObjectId objectId = new ObjectId(myId);
+        Optional<JournalEntry> journalEntry = journalEntryService.findById(objectId);
+
+        if (journalEntry.isPresent()) {
+            return new ResponseEntity<>(journalEntry.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
+
+
     @DeleteMapping("/id/{id}")
-    public boolean deleteJournalEntriesbyID(@PathVariable ObjectId id)
+    public ResponseEntity<?>deleteJournalEntriesbyID(@PathVariable ObjectId id)
     {
         journalEntryService.deleteById(id);
-        return true;
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @PutMapping("/id/{id}")
-    public JournalEntry UpdateJournalEntriesbyId(@PathVariable ObjectId id , @RequestBody JournalEntry newEntry){
+    public ResponseEntity<?> UpdateJournalEntriesbyId(@PathVariable ObjectId id , @RequestBody JournalEntry newEntry){
         JournalEntry old = journalEntryService.findById(id).orElse(null);
         if(old != null){
             old.setTitle(newEntry.getTitle() != null && !newEntry.getTitle().equals("") ? newEntry.getTitle() : old.getTitle());
             old.setContent(newEntry.getContent() != null && !newEntry.getContent().equals("") ? newEntry.getContent() : old.getContent());
+            journalEntryService.saveEntry(old);
+            return new ResponseEntity<>(old , HttpStatus.OK);
         }
-        journalEntryService.saveEntry(old);
-        return old;
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/com/example/journalApp/repository/JournalEntryRepository.java
+++ src/main/java/com/example/journalApp/repository/JournalEntryRepository.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface JournalEntryRepository extends MongoRepository<JournalEntry, ObjectId> {
 
-    ObjectId id(ObjectId id);
+
 }
