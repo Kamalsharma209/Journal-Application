47f8fa62108752d45bfedb58f1b99a114dc868dc
Your Name <Kamalsharma4769@gmail.com>
2026-01-08 19:05:42 +0530

    Making logs file to identify the errors and all

--- src/main/java/com/example/journalApp/service/UserService.java
+++ src/main/java/com/example/journalApp/service/UserService.java
@@ -2,17 +2,20 @@ package com.example.journalApp.service;
 
 import com.example.journalApp.entity.User;
 import com.example.journalApp.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-@Service
+@Component
+@Slf4j
 public class UserService {
 
     @Autowired
@@ -21,9 +24,13 @@ public class UserService {
     private static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     public void saveNewUser(User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setRoles(Arrays.asList("USER"));
-        userRepository.save(user);
+        try {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            user.setRoles(Arrays.asList("USER"));
+            userRepository.save(user);
+        } catch (Exception e) {
+            log.error("Error occured at {} :",user.getUsername(),e);
+        }
     }
     public void saveAdmin(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
--- src/test/java/com/example/journalApp/UserArgumentsProvider.java
+++
@@ -1,5 +0,0 @@
-package com.example.journalApp;
-
-public class UserArgumentsProvider {
-
-}
