bb3f444238ffdd2e12a146afcbe6031a472ccfb9
Your Name <Kamalsharma4769@gmail.com>
2026-01-14 11:30:50 +0530

    some changes in app cache and api services

--- src/main/java/com/example/journalApp/Cache/AppCache.java
+++ src/main/java/com/example/journalApp/Cache/AppCache.java
@@ -6,21 +6,28 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Component
 public class AppCache {
-    public Map<String,String> APP_CACHE;
+
+    public enum keys{
+        Weather_API;
+    }
+
+    public Map<String,String> appCache ;
 
     @Autowired
     private ConfigJournalAppRepository configJournalAppRepository;
 
     @PostConstruct
     public void init(){
+        appCache = new HashMap<>();
         List<ConfigJournalAppEntity> all = configJournalAppRepository.findAll();
         for(ConfigJournalAppEntity configJournalAppEntity : all){
-            APP_CACHE.put(configJournalAppEntity.getKey(), configJournalAppEntity.getValue());
+            appCache.put(configJournalAppEntity.getKey(), configJournalAppEntity.getValue());
         }
     }
 }
